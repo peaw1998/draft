b5c85d8448255d4a4437ba6df2401a1ce002d4ab
peaw1998 <m.peaw5500@gmail.com>
2020-04-20 00:22:42 +0700

    frontend login css

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,10 +1,53 @@
 import React from "react";
-import { Button, Card } from "react-bootstrap";
+import "../App.css";
+import { Button, Card, Form, Col } from "react-bootstrap";
+import LoginButton from "./LoginFacebookButton";
 
 const Login = (props) => {
   return (
     <>
-      <div className="bg center_page">123</div>
+      <div className="bg center_page">
+        <Card bg="dark" style={{ width: "30rem", height: "30rem" }}>
+          <Card.Header className="font" style={{ fontSize: 25 }}>
+            Login
+          </Card.Header>
+          <Card.Body className="login">
+            <Form style={{ width: "60%" }}>
+              <Form.Row>
+                <Form.Group as={Col} controlId="validationCustom01">
+                  <Form.Label className="font2">E-mail</Form.Label>
+                  <Col>
+                    <Form.Control
+                      placeholder="E-mail"
+                      className="font"
+                      style={{ marginTop: 5, marginBottom: 5 }}
+                      // onChange={(e) => setName(e.target.value)}
+                    />
+                  </Col>
+                </Form.Group>
+              </Form.Row>
+              <Form.Row>
+                <Form.Group as={Col} controlId="validationCustom01">
+                  <Form.Label className="font2">Password</Form.Label>
+                  <Col>
+                    <Form.Control
+                      placeholder="Password"
+                      type="password"
+                      className="font"
+                      style={{ marginTop: 5, marginBottom: 5 }}
+                      // onChange={(e) => setName(e.target.value)}
+                    />
+                  </Col>
+                </Form.Group>
+              </Form.Row>
+            </Form>
+            <Button variant="success" style={{ marginBottom: 10 }}>
+              Login
+            </Button>
+            <LoginButton />
+          </Card.Body>
+        </Card>
+      </div>
     </>
   );
 };
--- frontend/src/components/LoginFacebookButton.js
+++ frontend/src/components/LoginFacebookButton.js
@@ -1,45 +1,50 @@
-import React from "react"
-import firebase from "../firebase"
+import React from "react";
+import firebase from "../firebase";
+import { Button } from "react-bootstrap";
 
 const LoginFacebookButton = () => {
-      let provider = new firebase.auth.FacebookAuthProvider()
+  let provider = new firebase.auth.FacebookAuthProvider();
 
-      return (
-            <button
-                  onClick={() => {
-                        firebase
-                              .auth()
-                              .signInWithPopup(provider)
-                              .then(function (result) {
-                                    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+  return (
+    <Button
+      onClick={() => {
+        firebase
+          .auth()
+          .signInWithPopup(provider)
+          .then(function (result) {
+            // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+            console.log(result);
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            // ...
 
-                                    var token = result.credential.accessToken
-                                    // The signed-in user info.
-                                    var user = result.user
-                                    // ...
-                                    if (result.isNewUser) {
-                                          //register
-                                    } else {
-                                    }
+            if (result.isNewUser) {
+              //register
+              //ระบบ login อื่น นอกจาก facebook
+              //
+            } else {
+              //login
+            }
 
-                                    console.log("token ", token)
-                                    console.log("user email", user.email)
-                              })
-                              .catch(function (error) {
-                                    // Handle Errors here.
-                                    var errorCode = error.code
-                                    var errorMessage = error.message
-                                    // The email of the user's account used.
-                                    var email = error.email
-                                    // The firebase.auth.AuthCredential type that was used.
-                                    var credential = error.credential
-                                    // ...
-                              })
-                  }}
-            >
-                  login with facebook
-            </button>
-      )
-}
+            console.log("token ", token);
+            console.log("user email", user.email);
+          })
+          .catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+          });
+      }}
+    >
+      Login with Facebook
+    </Button>
+  );
+};
 
-export default LoginFacebookButton
+export default LoginFacebookButton;
